001e224bfb0bb628a6eb2c0df48e8ab5125f2375
unknown <hoff.ali@inbox.ru>
2022-03-23 18:52:28 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         still not woorking
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          u suck
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
